e5892be3f0f1ba3e3f394909b96809c9b23eb76a
camshaft <bytheway.cameron@gmail.com>
2015-01-23 15:30:06 -0700

    move msgid out of response tuple

--- src/annex_marshal_msgpack.erl
+++ src/annex_marshal_msgpack.erl
@@ -6,8 +6,8 @@
 -export([cast/4]).
 -export([response/2]).
 -export([response/3]).
--export([error/2]).
 -export([error/3]).
+-export([error/4]).
 
 -export([encode/1]).
 -export([decode/1]).
@@ -45,24 +45,24 @@ decode(Bin) ->
     {error, _} = Error ->
       Error;
     {ok, [0, MsgID, Action, Arguments]} ->
-      {ok, {call, MsgID, Action, Arguments}};
+      {ok, MsgID, {call, Action, Arguments}};
     {ok, [0, MsgID, Action, Arguments, Meta]} ->
-      {ok, {call, MsgID, Action, Arguments, Meta}};
+      {ok, MsgID, {call, Action, Arguments, Meta}};
 
     {ok, [1, MsgID, Action, Arguments]} ->
-      {ok, {cast, MsgID, Action, Arguments}};
+      {ok, MsgID, {cast, MsgID, Action, Arguments}};
     {ok, [1, MsgID, Action, Arguments, Meta]} ->
-      {ok, {cast, MsgID, Action, Arguments, Meta}};
+      {ok, MsgID, {cast, Action, Arguments, Meta}};
 
     {ok, [2, MsgID, Response]} ->
-      {ok, {response, MsgID, Response}};
+      {ok, MsgID, {response, Response}};
     {ok, [2, MsgID, Response, Meta]} ->
-      {ok, {response, MsgID, Response, Meta}};
+      {ok, MsgID, {response, Response, Meta}};
 
     {ok, [3, MsgID, Code, Message]} ->
-      {ok, {error, MsgID, Code, Message}};
+      {ok, MsgID, {error, Code, Message}};
     {ok, [3, MsgID, Code, Message, Meta]} ->
-      {ok, {error, MsgID, Code, Message, Meta}};
+      {ok, MsgID, {error, Code, Message, Meta}};
 
     {ok, Other} ->
       {error, {unexpected_response, Other}}
