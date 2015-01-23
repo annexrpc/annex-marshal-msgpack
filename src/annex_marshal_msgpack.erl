544f5f7efc90c1596d3c629f9d8365a3e2160b82
camshaft <bytheway.cameron@gmail.com>
2015-01-23 15:16:45 -0700

    use new spec

--- src/annex_marshal_msgpack.erl
+++ src/annex_marshal_msgpack.erl
@@ -1,44 +1,69 @@
 -module(annex_marshal_msgpack).
 
--export([encode/3]).
--export([encode/5]).
+-export([call/3]).
+-export([call/4]).
+-export([cast/3]).
+-export([cast/4]).
+-export([response/2]).
+-export([response/3]).
+-export([error/2]).
+-export([error/3]).
+
+-export([encode/1]).
 -export([decode/1]).
-
--define(REQ_ENUM(Type), case Type of
-                          call -> 0;
-                          cast -> 1
-                        end).
-
--define(RES_ENUM(Type), case Type of
-                          response -> 0;
-                          error -> 1
-                        end).
 
 -define(OPTIONS, [
   {allow_atom, pack},
   {format, map}
 ]).
 
-%% [type, msgid, module, function, params]
-encode(MsgID, Type, Response) ->
-  msgpack:pack([?RES_ENUM(Type), MsgID, Response], ?OPTIONS).
+call(MsgID, Action, Arguments) ->
+  encode([0, MsgID, Action, Arguments]).
+call(MsgID, Action, Arguments, Meta) ->
+  encode([0, MsgID, Action, Arguments, Meta]).
 
-encode(MsgID, Type, Module, Function, Arguments) ->
-  msgpack:pack([?REQ_ENUM(Type), MsgID, Module, Function, Arguments], ?OPTIONS).
+cast(MsgID, Action, Arguments) ->
+  encode([1, MsgID, Action, Arguments]).
+cast(MsgID, Action, Arguments, Meta) ->
+  encode([1, MsgID, Action, Arguments, Meta]).
 
-%% [type, msgid, result]
+response(MsgID, Response) ->
+  encode([2, MsgID, Response]).
+response(MsgID, Response, Meta) ->
+  encode([2, MsgID, Response, Meta]).
+
+error(MsgID, Response) ->
+  encode([3, MsgID, Response]).
+error(MsgID, Response, Meta) ->
+  encode([3, MsgID, Response, Meta]).
+
+encode(Args) ->
+  msgpack:pack(Args, ?OPTIONS).
+
 decode(Bin) ->
   case msgpack:unpack(Bin, [{format, map}]) of
     {error, _} = Error ->
       Error;
-    {ok, [0, MsgID, Result]} ->
-      {ok, MsgID, Result};
-    {ok, [1, MsgID, Error]} ->
-      {error, MsgID, Error};
-    {ok, [0, MsgID, Module, Function, Arguments]} ->
-      {call, MsgID, Module, Function, Arguments};
-    {ok, [1, MsgID, Module, Function, Arguments]} ->
-      {cast, MsgID, Module, Function, Arguments};
+    {ok, [0, MsgID, Action, Arguments]} ->
+      {ok, {call, MsgID, Action, Arguments}};
+    {ok, [0, MsgID, Action, Arguments, Meta]} ->
+      {ok, {call, MsgID, Action, Arguments, Meta}};
+
+    {ok, [1, MsgID, Action, Arguments]} ->
+      {ok, {cast, MsgID, Action, Arguments}};
+    {ok, [1, MsgID, Action, Arguments, Meta]} ->
+      {ok, {cast, MsgID, Action, Arguments, Meta}};
+
+    {ok, [2, MsgID, Response]} ->
+      {ok, {response, MsgID, Response}};
+    {ok, [2, MsgID, Response, Meta]} ->
+      {ok, {response, MsgID, Response, Meta}};
+
+    {ok, [3, MsgID, Response]} ->
+      {ok, {error, MsgID, Response}};
+    {ok, [3, MsgID, Response, Meta]} ->
+      {ok, {error, MsgID, Response, Meta}};
+
     {ok, Other} ->
       {error, {unexpected_response, Other}}
   end.
